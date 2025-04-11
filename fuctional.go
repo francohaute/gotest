06f2b4f5a12a6a2b165c0efcf4cf3128e401b414
Franco Hauteville <francohauteville@gmail.com>
2025-04-10 21:30:15 -0300

    functional test

---
+++ fuctional.go
@@ -0,0 +1,38 @@
+package main
+
+func Map[T1, T2 any](s []T1, f func(T1) T2) []T2 {
+	r := make([]T2, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
+	r := initializer
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
+// Test version with append and with a capacity of the len
+func Filter[T any](s []T, f func(T) bool) []T {
+	var r []T
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
+
+func FilterWithoutAppend[T any](s []T, f func(T) bool) []T {
+	r := make([]T, 0, len(s))
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
