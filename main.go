db1db1b78bb43125658ae20e45c685e2125785a4
Franco Hauteville <francohauteville@gmail.com>
2025-04-15 20:41:19 -0300

    Errors

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
 import "fmt"
-import "time"
 
 func main() {
-	slice := make([]int, 100_000_000, 100_000_000)
-	for i := range slice {
-		slice[i] = i
-	}
-	start := time.Now()
-	Filter(slice, func(i int) bool {
-		return (i%2 == 0)
-	})
-	fmt.Println("Duration filter with: ", time.Since(start))
-	start2 := time.Now()
-	FilterWithoutAppend(slice, func(i int) bool {
-		return (i%2 == 0)
-	})
-	fmt.Println("Duration filter without: ", time.Since(start2))
+	fmt.Println("Hello world!")
 }
